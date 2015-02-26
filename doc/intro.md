cf3bdf3f4303e35101f05c44e41ee043a2c3d3e7
Rodrigo Taboada <rtaboada10@gmail.com>
2015-02-25 23:10:01 -0300

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to challenge
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/challenge/core_test.clj
@@ -0,0 +1,7 @@
+(ns challenge.core-test
+  (:require [clojure.test :refer :all]
+            [challenge.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
