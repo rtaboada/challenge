5f8b56b14f3031de9b650b4d683e63d07ff7e2fc
Rodrigo Taboada <rtaboada10@gmail.com>
2015-02-28 17:38:10 -0300

    Improved comments and refactory of fraudulent function.

--- src/challenge/core.clj
+++ src/challenge/core.clj
@@ -7,10 +7,11 @@
   (map #(string/split %1 #" ")
        (string/split (slurp file-name) #"\n")))
 
+
 (def g-test (mapv (fn [xs] 
                     (mapv #(if (zero? %1) 
                              1e10M 
-                             1) 
+                             %1) 
                           xs)) 
                   [[0  8  15  9   0  0  0   0]
                    [8  0  13  0   0  0  0   0]
@@ -27,13 +28,15 @@
      (if (= i j)
        0
        (if (zero? k)
-         (get-in g [(dec j) (dec i)])
+         (get-in g [(dec i) (dec j)])
          (min (shortest-path g i j (dec k))
               (+ (shortest-path g i k (dec k))
                  (shortest-path g k j (dec k)))))))))
 
 (defn floyd-warshall
-  "All pairs shortest path distance graph algorithm"
+  "All pairs shortest path distance graph algorithm,
+   returns a distance matrix where the line and columns represent
+   the same vertices as the adjacency matrix."
   [g]
   (let [v (count g)]
     (partition v v
@@ -46,23 +49,28 @@
   (map #(/ 1 (apply + %1)) dist))
 
 
-(defn initial-score [g]
+(defn initial-score 
+  "Returns a seq with the initial score of all vertices in the graph."
+  [g]
   (-> g
       floyd-warshall
       closeness-centrality))
 
 
 (defn factor 
-  "Coefficient that varies"
+  "Scaling factor applied to a vertex score,
+   k is the distance between the fraudulent vertex and the other vertex."
   [k]
   (rationalize (- 1 (Math/pow 1/2 k))))
 
 
 (defn fraudulent 
-  "Marks a vertex v as fraudulent, the vertex score will be zero and every other vertex's score will be changed by a factor dependent on their distance to the fraudulent vertex."
+  "Marks a vertex v as fraudulent, v score will be zero and every 
+   other vertex's score will be changed by a factor dependent on 
+   their distance to the fraudulent vertex."
   [score dist v]
-  (map * 
-       score 
-       (map factor (nth dist v))))
-
+  (->> v
+       (nth dist)
+       (map factor)
+       (map * score)))
 
