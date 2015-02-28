b727ea4ab073e5b3209432d243662e9873982597
Rodrigo Taboada <rtaboada10@gmail.com>
2015-02-28 01:53:51 -0300

    Initial implementation of Floyd-Warshall algorithm, using an adjacency matrix to represent the graph.

--- src/challenge/core.clj
+++ src/challenge/core.clj
@@ -1,6 +1,45 @@
-(ns challenge.core)
+(ns challenge.core
+  (:require [clojure.string :as string]
+            [clojure.set :as set]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn read-file [file-name]
+  (map #(string/split %1 #" ")
+       (string/split (slurp file-name) #"\n")))
+
+(def g-test (mapv (fn [xs] 
+                    (mapv #(if (zero? %1) 
+                             1e10M 
+                             1) 
+                          xs)) 
+                  [[0  8  15  9   0  0  0   0]
+                   [8  0  13  0   0  0  0   0]
+                   [15 13 0   0   6  0  5   0]
+                   [9  0  0   0  13  0  0   0]
+                   [0  0  6  13   0  7  0   0]
+                   [0  0  0   0   7  0  9   4]
+                   [0  0  5   0   0  9  0   18]
+                   [0  0  0   0   0  4  18  0]]))
+
+(def shortest-path
+  (memoize 
+   (fn [g i j k]
+     (if (= i j)
+       0
+       (if (zero? k)
+         (get-in g [(dec j) (dec i)])
+         (min (shortest-path g i j (dec k))
+              (+ (shortest-path g i k (dec k))
+                 (shortest-path g k j (dec k)))))))))
+
+(defn floyd-warshall
+  "All pairs shortest path distance graph algorithm"
+  [g]
+  (let [v (count g)]
+    (partition v v
+               (for [x (range 1 (inc v)) y (range 1 (inc v)) ]
+                 (shortest-path g x y v)))))
+
+
+
+
