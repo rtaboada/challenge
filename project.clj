1f78086a8c22aa75a603fdb99eae2b2680efdf59
Rodrigo Taboada <rtaboada10@gmail.com>
2015-03-01 17:53:18 -0300

    Adds Pedestal dependencies and boilerplate.

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [io.pedestal/pedestal.service "0.3.1"]
+                 [io.pedestal/pedestal.jetty "0.3.1"]]
+  :min-lein-version "2.0.0"
+  :resource-paths ["config" "resources"]
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "challenge.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.3.1"]]}}
+  :main ^{:skip-aot true} challenge.server)
