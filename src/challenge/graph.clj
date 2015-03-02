124db188f2b9ebc84faa1393391a2efa420ce034
Rodrigo Taboada <rtaboada10@gmail.com>
2015-03-02 03:08:30 -0300

    Fixes bug in shortest-path.

--- src/challenge/graph.clj
+++ src/challenge/graph.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as string]))
 
 
-(def max-dist 1e10M)
+(def max-dist 1e10N)
 
 (defn- lines [content]
   (string/split content #"\n"))
@@ -20,13 +20,20 @@
 (defn read-edge-file [file-name]
   (map parse-edge (read-file file-name)))
 
+
 (defn vertices
   "Return the set of vertices defined by edges."
   [edges]
-  (set (apply concat edges)))
+  (sort (set (apply concat edges))))
 
-(defn create-adjacency-matrix [edges]
-  (let [neighbors (group-by first edges)
+
+(defn create-adjacency-matrix 
+  "Creates an adjacency matrix given the edges of the graph. 
+   Some assumptions: - the graph is undirected.
+                     - the ids are compact, ie. without holes.
+                     - first id is zero and last id is (dec (count vertices))"
+  [edges]
+  (let [neighbors (sort (group-by first edges))
         empty-row (vec (repeat (count (vertices edges))
                                max-dist))]
     (mapv #(apply assoc empty-row (mapcat vector 
@@ -37,13 +44,14 @@
 (def ^:private shortest-path
   (memoize
    (fn [g i j k]
-      (if (= i j)
-        0
-        (if (zero? k)
-          (get-in g [i j])
-          (min (shortest-path g i j (dec k))
-               (+ (shortest-path g i k (dec k))
-                  (shortest-path g k j (dec k)))))))))
+     (if (= i j)
+       0
+       (if (neg? k)
+         (get-in g [i j])
+         (min (shortest-path g i j (dec k))
+              (+ (shortest-path g i k (dec k))
+                 (shortest-path g k j (dec k)))))))))
+
 
 (defn floyd-warshall
   "All pairs shortest path distance graph algorithm,
@@ -54,6 +62,7 @@
     (partition v v
                (for [x (range 0 v) y (range 0 v)]
                  (shortest-path g x y (dec v))))))
+
 
 (defn closeness-centrality
   "Returns the closeness of every vertex in the graph, dist is a distance matrix.
