ed5e65cdf741ffd23d6977f140cf5b9963fe1991
Rodrigo Taboada <rtaboada10@gmail.com>
2015-03-07 20:32:33 -0300

    Adds tests to check if the graph generators are creating the correct number of edges.

--- test/challenge/graph_generators_test.clj
+++ test/challenge/graph_generators_test.clj
@@ -5,7 +5,9 @@
             [challenge.graph :refer [vertices]]
             [challenge.graph-generators :refer :all]))
 
+;; ## Unit tests ##
 
+;; Checks if the line graph function is creating the spected edges.
 (deftest line-graph-test
   (testing "Line graph with 5 vertices"
     (let [graph (line-graph-fn 5)]
@@ -14,6 +16,7 @@
       (is (= (count (vertices graph)) 5)))))
 
 
+;; Tests that the ring graph function is creating the correct edges. 
 (deftest ring-graph-generation-test
   (testing "Ring graph with 9 vertices."
     (let [graph (ring-graph-fn 9)]
@@ -22,6 +25,7 @@
       (is (= (count (vertices graph)) 9)))))
 
 
+;; Tests if the star graph function is generating the correct edges.
 (deftest star-graph-generation-test
   (testing "Star graph with 8 vertices" 
     (let [graph (star-graph-fn 8)]
@@ -29,20 +33,53 @@
              [[0 1] [1 0] [0 2] [2 0] [0 3] [3 0] [0 4] [4 0] [0 5] [5 0] [0 6] [6 0] [0 7] [7 0]]))
       (is (= (count (vertices graph)) 8)))))
 
+;; ## Property base tests ##
 
 ;; Check if the number of edges is the expected for a complete graph.
 (defspec number-edges-complete-graph
   (prop/for-all
    [n num-vertex]
    (let [edges (complete-graph-fn n)]
-     ;; Number of edges should be n*(n-1)/2 in a complete graph. 
+     ;; Number of edges should be `n * (n - 1) / 2` in a complete graph. 
      ;; But because of the way we represent the edges, they are counted twice.
      (= (* n (dec n)) (count edges)))))
 
 
-;; Check if the number of vertices is the one expected for a complete graph.
-(defspec number-vertices-in-complete-graph-is-n
+;; Checks if the number of vertices is the one expected for a complete graph.
+(defspec number-vertices-in-complete-graph
   (prop/for-all 
    [n num-vertex]
    (= n (count (vertices (complete-graph-fn n))))))
+
+
+;; Checks if the number of edges is the expected for a line graph.
+(defspec number-edges-line-graph
+  (prop/for-all
+   [n num-vertex]
+   (let [edges (line-graph-fn n)]
+     ;; Number of edges for a line graph is `n - 1`.
+     ;; But because of the way we represent undirected edges, the
+     ;; number should be twice that.
+     (= (* 2 (dec n))
+        (count edges)))))
+
+
+;; Checks if the number of generated edges is correct for a ring graph.
+(defspec number-edges-ring-graph
+  (prop/for-all
+   [n num-vertex]
+   ;; Number of edges in a ring graph is `n`. But we are representing
+   ;; undirected edges with two values `[v1 v2]` and `[v2 v1]` so the
+   ;; total number of edges should be `2 * n`.
+   (= (* 2 n)
+      (count (ring-graph-fn n)))))
+
+
+;; Checks the number of generated edges in the star graph.
+(defspec number-edges-star-graph
+  (prop/for-all
+   [n num-vertex]
+   ;; Edges in a star graph in our edge representation: `2 * (n - 1)`
+   (= (* 2 (dec n)) 
+      (count (star-graph-fn n)))))
 
